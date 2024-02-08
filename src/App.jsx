0999b3057cc4beb3e159251a3a67ca297e2e71cc
saeed padkan <s.padkan@gmail.com>
2024-02-08 15:22:21 +0100

    add pagination and search

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Structure from "../src/components/structure";
 import News from "./components/news";
 import Footer from "./components/footer";
 import { useEffect, useState } from "react";
+const stylebtn = {
+  width: "6rem",
+  padding: "0.5rem",
+};
 
 export default function App() {
   const [news, setNews] = useState([]);
-  const newsApi =
-    "http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=6&page=1";
+  const [page, setPage] = useState(1);
+  const [search, updateSearch] = useState("");
+  const [query, setQuery] = useState("tags=front_page");
+
+  const newsApi = `http://hn.algolia.com/api/v1/search?${query}&hitsPerPage=6&page=${page}`;
 
   const fetchData = async () => {
     try {
@@ -19,13 +26,49 @@ export default function App() {
     }
   };
 
+  const handledSearch = () => {
+    const q = search && search !== "" ? `query=${search}` : "tags=front_page";
+    setQuery(q);
+  };
+
+  const handledNextPage = () => {
+    if (page >= 4) return;
+    setPage((page) => page + 1);
+  };
+  const handledPrevPage = () => {
+    if (page <= 1) return;
+    setPage((page) => page - 1);
+  };
   useEffect(() => {
-    fetchData(newsApi);
-  }, [news]);
+    fetchData();
+  }, [newsApi]);
 
   return (
     <>
       <Structure news={news}>
+        <p>
+          {page > 1 && (
+            <button style={stylebtn} onClick={handledPrevPage}>
+              prev
+            </button>
+          )}
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <button style={stylebtn} onClick={handledNextPage}>
+            next
+          </button>
+          <label htmlFor="search">Search</label>
+          <input
+            type="text"
+            value={search}
+            name="search"
+            id="search"
+            onChange={(e) => updateSearch(e.target.value)}
+          />
+          <button style={stylebtn} onClick={handledSearch}>
+            Go!
+          </button>
+        </p>
+
         <News news={news} />
         <Footer />
       </Structure>
